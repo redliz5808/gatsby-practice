b67e4ac78a1fdc4e133894e140850499bcba68a1
Beth Franklin <elizabethwfranklin@gmail.com>
2023-12-16 21:37:22 -0500

    Test About page

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import * as React from "react"
+import { Link } from "gatsby";
+
+const About = () => {
+    return (
+        <div>
+            <h1>About Me!!!!!</h1>
+            <p>This is a page about me!</p>
+            <Link to="/">Home</Link>
+        </div>
+    )
+}
+
+export default About;
