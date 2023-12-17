b5910f0d75f26db1e5c7f20a25384fa47fceae73
Beth Franklin <elizabethwfranklin@gmail.com>
2023-12-16 18:04:53 -0800

    Initial Commit

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Practice Site`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-styled-components"]
+};
