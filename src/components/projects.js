74df5c6331e6bd37d0de6056fd59089010df393d
redliz5808 <elizabethwfranklin@gmail.com>
2024-02-08 19:45:27 -0500

    Insert Swiper for project cards

---
+++ src/components/projects.js
@@ -0,0 +1,53 @@
+const Projects = [
+  {
+    projectName: "Gryt Health",
+    projectImageUrl: "",
+    projectWireframeUrl: "",
+    projectCodeUrl: "",
+    projectDemoUrl: "",
+  },
+  {
+    projectName: "MS Health Equity Alliance",
+    projectImageUrl: "",
+    projectWireframeUrl: "",
+    projectCodeUrl: "",
+    projectDemoUrl: "",
+  },
+  {
+    projectName: "The Advocacy Exchange",
+    projectImageUrl: "",
+    projectWireframeUrl: "",
+    projectCodeUrl: "",
+    projectDemoUrl: "",
+  },
+  {
+    projectName: "Lung Cancer Health Equity",
+    projectImageUrl: "",
+    projectWireframeUrl: "",
+    projectCodeUrl: "",
+    projectDemoUrl: "",
+  },
+  {
+    projectName: "A Thousand Words",
+    projectImageUrl: "",
+    projectWireframeUrl: "",
+    projectCodeUrl: "",
+    projectDemoUrl: "",
+  },
+  {
+    projectName: "Weather App",
+    projectImageUrl: "",
+    projectWireframeUrl: "",
+    projectCodeUrl: "",
+    projectDemoUrl: "",
+  },
+  {
+    projectName: "Pomodoro To Do App",
+    projectImageUrl: "",
+    projectWireframeUrl: "",
+    projectCodeUrl: "",
+    projectDemoUrl: "",
+  },
+];
+
+export default Projects;
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,16 +2,33 @@ import * as React from "react";
 import { Link } from "gatsby";
 import Layout from "../components/layout";
 import * as Styles from "../components/styles";
+import Projects from "../components/projects";
+
+//Swiper Import
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Parallax, EffectCards } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/parallax";
+import "swiper/css/effect-cards";
+
+let SwiperContent = Projects.map((project) => {
+  return <SwiperSlide>{project.projectName}</SwiperSlide>;
+});
+
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Projects">
-      <h2>
-        Congratulations
-        <br />
-        <span>— you just made a Gatsby site! 🎉🎉🎉</span>
-      </h2>
-      <Link to="/resume">My Resume!</Link>
+      <Swiper
+        modules={[EffectCards]}
+        effect="cards"
+        spaceBetween={50}
+        slidesPerView={5}
+        onSlideChange={() => console.log("slide change")}
+        onSwiper={(swiper) => console.log(swiper)}
+      >
+        {SwiperContent}
+      </Swiper>
     </Layout>
   );
 };
